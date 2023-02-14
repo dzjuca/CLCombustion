697f74401c25899919b01ddaa653d763280d5ffc
Daniel Z. Juca <daniel3959@hotmail.com>
2023-02-14 16:12:16 +0000

    the r_rfH2O_Fcn.m file compute the kineticto dry reorming with H2O

---
+++ kinetic/r_WGS_Fcn.m
@@ -0,0 +1,21 @@
+function r_WGS = r_WGS_Fcn(PCO,PCO2, PH2, T)
+% -------------------------------------------------------------------------
+    % r_WGS_Fcn is a function that calculates the reaction rate of ...
+    % ----------------------------| input |--------------------------------
+    % T = temperature of the system                                     [K]
+    % R = Universal Gas Constant                                   [J/molK]
+    % KCO_rf_H2O  =  kinetic constant                               [bar-1]
+    % KH2_rf_H2O  =  kinetic constant                             [bar-0.5]
+    % KH2O_rf_H2O =  kinetic constant                                    []
+    % Kp_rf_H2O   =  kinetic constant                                [bar2]
+    % k_rf_H2O    =  kinetic constant                 [kmol/kgNi s bar0.25]
+    % ----------------------------| output |-------------------------------
+    %   r_WGS = the reaction rate                           [kmol/kgNi s]
+% -------------------------------------------------------------------------
+
+
+
+r_WGS = 0;
+
+
+end
