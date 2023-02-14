c003fc0f06902882a4c9b554dbdb622dd4ba420d
Daniel Z. Juca <daniel3959@hotmail.com>
2023-02-14 14:24:57 +0000

    the main.m file is created

---
+++ main.m
@@ -0,0 +1,29 @@
+% -------------------------------------------------------------------------
+% ------------------------ FLUIDIZED BED REACTOR --------------------------
+%                    Chemical Looping Combustion - CLC
+%     Author: Daniel Z. Juca
+% ------------------------ | initiation | ---------------------------------
+    close all
+    clear
+    clc
+% ---------- global constants ---------------------------------------------
+    Global = globalData();
+    NoN  = (1:Global.n*Global.Num_esp);
+% ---------- initial condition --------------------------------------------
+    u0   = initialConditions(Global);
+% ---------- time simulation (s) ------------------------------------------
+    t0   = 0.0; 
+    tf   = 30; 
+    tout = linspace(t0,tf,100)';
+% ---------- Implicit (sparse stiff) integration --------------------------
+    reltol   = 1.0e-6; abstol = 1.0e-6;  
+    options  = odeset('RelTol',reltol,'AbsTol',abstol,'NonNegative',NoN);
+    S        = JPatternFcn(Global);
+    options  = odeset(options,'JPattern',S); 
+    pdeModel = @(t,u)pdeFcn(t,u,Global);
+    [t,u]    = ode15s(pdeModel,tout,u0,options);  
+% -----
+    [CiBW,CiE] = SkinFcnMac(t,u,Global);
+% ----
+    skinTemperatureFcn(t, u, Global)
+% ---------------------------| End Program |-------------------------------
