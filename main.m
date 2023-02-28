65e090feae5b46b80a1c1fac57523454e2af691c
Daniel Z. Juca <daniel3959@hotmail.com>
2023-02-28 20:17:34 +0000

    kinetics functions update

--- main.m
+++ main.m
@@ -13,7 +13,7 @@
     u0   = initialConditions(Global);
 % ---------- time simulation (s) ------------------------------------------
     t0   = 0.0; 
-    tf   = 7*60; 
+    tf   = 60; 
     tout = linspace(t0,tf,100)';
 % ---------- Implicit (sparse stiff) integration --------------------------
     reltol   = 1.0e-6; abstol = 1.0e-6;  
@@ -24,13 +24,13 @@
     [t,u]    = ode15s(pdeModel,tout,u0,options);  
 %% -----
     graphsFmgAllSpeciesFcn(t, u, Global)
-%     graphs_C_g_b_Fcn(t,u, Global)
-%     graphs_C_g_e_Fcn(t,u, Global)
-%     graphs_C_s_w_Fcn(t,u, Global)
-%     graphs_C_s_e_Fcn(t,u, Global)
-%     graphsCgbAllSpeciesFcn(t,u, Global)
-%     graphsCgeAllSpeciesFcn(t,u, Global)
-%     graphsCswAllSpeciesFcn(t,u, Global)
-%     graphsCseAllSpeciesFcn(t,u, Global)
+    graphs_C_g_b_Fcn(t,u, Global)
+    graphs_C_g_e_Fcn(t,u, Global)
+    graphs_C_s_w_Fcn(t,u, Global)
+    graphs_C_s_e_Fcn(t,u, Global)
+    graphsCgbAllSpeciesFcn(t,u, Global)
+    graphsCgeAllSpeciesFcn(t,u, Global)
+    graphsCswAllSpeciesFcn(t,u, Global)
+    graphsCseAllSpeciesFcn(t,u, Global)
 % ---
 % ---------------------------| End Program |-------------------------------
